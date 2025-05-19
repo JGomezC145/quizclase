ecfbe8f05367865e26c477c07e7e820e09ee4c1b
JGomezC145 <jgomezcyn@outlook.com>
2025-05-19 17:58:51 -0600

    Update main.c to fill rand nums to matrix.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h> // Para usar srand y rand
 
 int main() {
     int m, n;
@@ -14,10 +15,20 @@ int main() {
         matriz[i] = (int *)malloc(n * sizeof(int));
     }
 
-    // Mensaje de confirmación
-    printf("Matriz de %d x %d creada en memoria dinámica.\n", m, n);
+    // Inicializar la semilla para números aleatorios
+    srand(time(NULL));
 
-    
+    // Llenar la matriz con valores aleatorios entre 1 y 99 (no incluye 0)
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            matriz[i][j] = (rand() % 99) + 1; // rango [1, 99]
+        }
+    }
+
+    // Mensaje de confirmación
+    printf("Matriz llenada con valores aleatorios distintos de 0.\n");
+
+    // Liberar memoria
     for (int i = 0; i < m; i++) {
         free(matriz[i]);
     }
