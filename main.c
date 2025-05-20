981089bd0e15d3e8858619e80dc69a9a5c3e156d
JGomezC145 <jgomezcyn@outlook.com>
2025-05-19 18:09:55 -0600

    Update main.c, add feature to resize matrix.

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main() {
     printf("Ingrese el número de columnas (n): ");
     scanf("%d", &n);
 
-    // Reservar memoria dinámica para la matriz
+    // Reservar memoria para la matriz original
     int **matriz = (int **)malloc(m * sizeof(int *));
     for (int i = 0; i < m; i++) {
         matriz[i] = (int *)malloc(n * sizeof(int));
@@ -18,15 +18,59 @@ int main() {
     // Inicializar la semilla aleatoria
     srand(time(NULL));
 
-    // Llenar la matriz con valores aleatorios entre 1 y 99 (no incluye 0)
+    // Llenar matriz original
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             matriz[i][j] = (rand() % 99) + 1;
         }
     }
 
-    // Imprimir la matriz
-    printf("\nMatriz generada:\n");
+    // Imprimir matriz original
+    printf("\nMatriz original:\n");
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            printf("%3d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    // Pedir nuevo tamaño
+    int m2, n2;
+    printf("\nIngrese el nuevo número de filas (m2): ");
+    scanf("%d", &m2);
+    printf("Ingrese el nuevo número de columnas (n2): ");
+    scanf("%d", &n2);
+
+    // Crear nueva matriz
+    int **nuevaMatriz = (int **)malloc(m2 * sizeof(int *));
+    for (int i = 0; i < m2; i++) {
+        nuevaMatriz[i] = (int *)malloc(n2 * sizeof(int));
+    }
+
+    // Copiar datos de la matriz original y rellenar nuevos espacios
+    for (int i = 0; i < m2; i++) {
+        for (int j = 0; j < n2; j++) {
+            if (i < m && j < n) {
+                nuevaMatriz[i][j] = matriz[i][j];  // Copiar existente
+            } else {
+                nuevaMatriz[i][j] = (rand() % 99) + 1;  // Rellenar nuevos espacios
+            }
+        }
+    }
+
+    // Liberar la matriz original
+    for (int i = 0; i < m; i++) {
+        free(matriz[i]);
+    }
+    free(matriz);
+
+    // Actualizar puntero y dimensiones
+    matriz = nuevaMatriz;
+    m = m2;
+    n = n2;
+
+    // Imprimir nueva matriz
+    printf("\nMatriz redimensionada:\n");
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
             printf("%3d ", matriz[i][j]);
